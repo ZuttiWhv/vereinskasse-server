7d78b9088120f6560b6e0887a7cad822a44b7b2c
toz <zuttiebay@web.de>
2026-04-06 20:11:57 +0200

    MediaController angelegt der Bilder dynamisch umrechnen kann beim Abruf

---
+++ src/main/java/de/tozwhv/vereinskasse/server/controller/MediaController.java
@@ -0,0 +1,36 @@
+package de.tozwhv.vereinskasse.server.controller;
+
+import de.tozwhv.vereinskasse.server.service.ImageResizeService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/media") // WICHTIG: Anderer Pfad als /media/
+public class MediaController {
+
+    private final ImageResizeService resizeService;
+
+    public MediaController(ImageResizeService resizeService) {
+        this.resizeService = resizeService;
+    }
+
+    @GetMapping("/{filename}")
+    public ResponseEntity<byte[]> getImage(
+            @PathVariable String filename,
+            @RequestParam(defaultValue = "80") int width) { // Standardbreite 100px
+
+        try {
+            byte[] imageBytes = resizeService.resizeImage(filename, width);
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .header("Cache-Control", "public, max-age=31536000") // 1 Jahr Browser-Cache!
+                    .body(imageBytes);
+        } catch (IOException _) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
