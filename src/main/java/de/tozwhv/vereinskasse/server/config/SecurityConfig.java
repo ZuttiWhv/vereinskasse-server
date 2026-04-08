4205023f66f53261623ef84e80d4a8bb99cec67a
toz <zuttiebay@web.de>
2026-04-08 21:24:24 +0200

    PasswordEncrypter als Bean ausgelagert

--- src/main/java/de/tozwhv/vereinskasse/server/config/SecurityConfig.java
+++ src/main/java/de/tozwhv/vereinskasse/server/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http
--- src/main/java/de/tozwhv/vereinskasse/server/service/AuthService.java
+++ src/main/java/de/tozwhv/vereinskasse/server/service/AuthService.java
@@ -7,7 +7,7 @@ import de.tozwhv.vereinskasse.server.repository.UserRepository;
 import io.jsonwebtoken.security.InvalidKeyException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +16,7 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final JwtProvider jwtProvider;
-    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final PasswordEncoder passwordEncoder;
 
     public AuthController.AuthResponse login(String username, String password) {
         User user = userRepository.findByUsername(username)
