f78f60649488efafcb842b05b2b716e5a6b8632a
toz <zuttiebay@web.de>
2026-04-10 18:39:49 +0200

    Marginaler Cleanup - Aliase entfernt

--- src/main/java/db/migration/V1_1__Prepopulate_User_And_Permissions.java
+++ src/main/java/db/migration/V1_1__Prepopulate_User_And_Permissions.java
@@ -125,7 +125,7 @@ public class V1_1__Prepopulate_User_And_Permissions extends BaseJavaMigration {
         // -----------------------------
         try (PreparedStatement stmt = connection.prepareStatement(
                 "INSERT INTO ROLE_PERMISSION (role_id, permission_id) " +
-                        "SELECT role.id, permission.id FROM role role, permission permission WHERE role.name='ADMIN' ")) {
+                        "SELECT role.id, permission.id FROM role, permission WHERE role.name='ADMIN' ")) {
             stmt.executeUpdate();
         }
     }
