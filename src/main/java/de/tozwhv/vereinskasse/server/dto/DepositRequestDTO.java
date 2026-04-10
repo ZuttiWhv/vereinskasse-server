a8060c18fc4ff2156ccc3418251355586972a843
toz <zuttiebay@web.de>
2026-04-11 00:33:34 +0200

    Automatic Refactoring and CleanUp

--- src/main/java/de/tozwhv/vereinskasse/server/dto/DepositRequestDTO.java
+++ src/main/java/de/tozwhv/vereinskasse/server/dto/DepositRequestDTO.java
@@ -2,6 +2,6 @@ package de.tozwhv.vereinskasse.server.dto;
 
 public record DepositRequestDTO(
         Long userId,
-        Integer amountInCents // Wir bleiben bei Cents für die Genauigkeit
+        Integer amountInCents
 ) {
 }
