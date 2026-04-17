1d53c6e24bd697d0073420c25eed19e5d93a9fd0
toz <zuttiebay@web.de>
2026-04-17 15:25:17 +0200

    Zu dumm zum CopyPasten

--- src/main/java/de/tozwhv/vereinskasse/server/repository/BillingGroupRepository.java
+++ src/main/java/de/tozwhv/vereinskasse/server/repository/BillingGroupRepository.java
@@ -1,12 +1,12 @@
 package de.tozwhv.vereinskasse.server.repository;
 
-import de.tozwhv.vereinskasse.server.modell.Category;
+import de.tozwhv.vereinskasse.server.modell.BillingGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface BillingGroupRepository extends JpaRepository<Category, Long> {
+public interface BillingGroupRepository extends JpaRepository<BillingGroup, Long> {
 
 }
 
