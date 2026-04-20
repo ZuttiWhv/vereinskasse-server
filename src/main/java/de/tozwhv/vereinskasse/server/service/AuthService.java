1d100dc98b402686b679d77c056d75ee554b3a5d
toz <zuttiebay@web.de>
2026-04-20 16:19:30 +0200

    Import

--- src/main/java/de/tozwhv/vereinskasse/server/service/AuthService.java
+++ src/main/java/de/tozwhv/vereinskasse/server/service/AuthService.java
@@ -1,7 +1,6 @@
 package de.tozwhv.vereinskasse.server.service;
 
 import de.tozwhv.vereinskasse.server.config.JwtProvider;
-import de.tozwhv.vereinskasse.server.controller.AuthController;
 import de.tozwhv.vereinskasse.server.dto.AuthResponse;
 import de.tozwhv.vereinskasse.server.modell.User;
 import de.tozwhv.vereinskasse.server.repository.UserRepository;
