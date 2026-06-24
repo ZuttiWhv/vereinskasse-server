25ad99af4a930fad858e6839460b595b35835433
toz <zuttiebay@web.de>
2026-06-24 14:16:47 +0200

    Integration von Fees (Gebühren)

--- src/main/java/de/tozwhv/vereinskasse/server/service/TransactionService.java
+++ src/main/java/de/tozwhv/vereinskasse/server/service/TransactionService.java
@@ -2,10 +2,9 @@ package de.tozwhv.vereinskasse.server.service;
 
 import de.tozwhv.vereinskasse.server.dto.TransactionDTO;
 import de.tozwhv.vereinskasse.server.dto.UserBalanceDTO;
-import de.tozwhv.vereinskasse.server.modell.Deposit;
-import de.tozwhv.vereinskasse.server.modell.Sale;
-import de.tozwhv.vereinskasse.server.modell.User;
+import de.tozwhv.vereinskasse.server.modell.*;
 import de.tozwhv.vereinskasse.server.repository.DepositRepository;
+import de.tozwhv.vereinskasse.server.repository.FeeRepository;
 import de.tozwhv.vereinskasse.server.repository.SaleRepository;
 import de.tozwhv.vereinskasse.server.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -20,12 +19,14 @@ public class TransactionService {
     private final SaleRepository saleRepository;
     private final DepositRepository depositRepository;
     private final UserRepository userRepository;
+    private final FeeRepository feeRepository;
 
 
-    public TransactionService(SaleRepository saleRepository, DepositRepository depositRepository, UserRepository userRepository) {
+    public TransactionService(SaleRepository saleRepository, DepositRepository depositRepository, UserRepository userRepository, FeeRepository feeRepository) {
         this.saleRepository = saleRepository;
         this.depositRepository = depositRepository;
         this.userRepository = userRepository;
+        this.feeRepository = feeRepository;
     }
 
 
@@ -37,6 +38,7 @@ public class TransactionService {
         // 1. Daten laden
         List<Sale> sales = saleRepository.findByUserAndCreatedAtBetweenOrderByCreatedAtDesc(user, actualStart, actualEnd);
         List<Deposit> deposits = depositRepository.findByUserAndCreatedAtBetweenOrderByCreatedAtDesc(user, actualStart, actualEnd);
+        List<Fee> fees = feeRepository.findByUserAndCreatedAtBetweenOrderByCreatedAtDesc(user, actualStart, actualEnd);
 
         List<TransactionDTO> history = new ArrayList<>();
 
@@ -44,15 +46,15 @@ public class TransactionService {
 
 
         sales.forEach(s -> {
-            String type = "SALE";
-            if (s.isUseVoucher()) type="REDEEM";
-            if (s.isVoucher()) type="VOUCHER";
+            TransactionType type = TransactionType.SALE;
+            if (s.isUseVoucher()) type=TransactionType.REDEEM;
+            if (s.isVoucher()) type=TransactionType.VOUCHER;
             history.add(new TransactionDTO(
                     s.getId(),
-                    type,
+                    type.name(),
                     s.getProduct().getName(),
                     s.getAmount(),
-                    s.getPrice(), // Gesammtsumme der Transaktion liegt bereits vor
+                    s.getPrice(),
                     s.getCreatedAt()
             ));
         });
@@ -60,11 +62,21 @@ public class TransactionService {
         // 3. Deposits konvertieren
         deposits.forEach(d -> history.add(new TransactionDTO(
                 d.getId(),
-                "DEPOSIT",
-                "Guthaben aufgeladen",
+                TransactionType.DEPOSIT.name(),
+                TransactionType.DEPOSIT.getDisplayName(),
                 1, // Menge bei Einzahlung ist immer 1
                 d.getAmount(),
                 d.getCreatedAt()
+        )));
+
+        // Fees konvertieren
+        fees.forEach(f -> history.add(new TransactionDTO(
+                f.getId(),
+                TransactionType.MONTHLY_CONTRIBUTION.name(),
+                TransactionType.MONTHLY_CONTRIBUTION.getDisplayName(),
+                1,
+                f.getAmount(),
+                f.getCreatedAt()
         )));
 
         // 4. Sortieren: Neueste zuerst
@@ -84,12 +96,14 @@ public class TransactionService {
             // 1. Daten laden
             List<Sale> sales = saleRepository.findByUserAndCreatedAtBetweenOrderByCreatedAtDesc(user, actualStart, actualEnd);
             List<Deposit> deposits = depositRepository.findByUserAndCreatedAtBetweenOrderByCreatedAtDesc(user, actualStart, actualEnd);
+            List<Fee> fees = feeRepository.findByUserAndCreatedAtBetweenOrderByCreatedAtDesc(user, actualStart, actualEnd);
 
             int allSales = sales.stream().map(Sale::getPrice).mapToInt(Integer::intValue).sum();
             int allDeposits = deposits.stream().map(Deposit::getAmount).mapToInt(Integer::intValue).sum();
+            int allFees = fees.stream().map(Fee::getAmount).mapToInt(Integer::intValue).sum();
 
             // 2. Kontostand zum Wunschdatum berechnen
-            long balanceAtDate = user.getBalance() + allSales - allDeposits;
+            long balanceAtDate = user.getBalance() + allSales - allDeposits +allFees;
 
             // 3. Filter-Logik für archivierte Benutzer:
             // Ein aktiver User wird IMMER hinzugefügt.
